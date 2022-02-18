c06ffc1cd682cb106b35609dc0bc596961d23f77
[Evgeniy Turin] <[turin.92@mail.ru]>
2022-02-18 12:51:28 +0400

    create FavoriteFragment and FragmentViewModel

--- presentation/src/main/java/com/example/pokemonfunproject/di/AppModules.kt
+++ presentation/src/main/java/com/example/pokemonfunproject/di/AppModules.kt
@@ -1,9 +1,12 @@
 package com.example.pokemonfunproject.di
 
+import com.example.pokemonfunproject.ui.favorites.FavoritesFragment
+import com.example.pokemonfunproject.ui.favorites.FavoritesViewModel
 import com.example.pokemonfunproject.ui.home.HomeViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val presentationModule = module {
     viewModel { HomeViewModel(get(), get(), get(), get(), get(), get()) }
+    viewModel { FavoritesViewModel(get(),get(),get(),get())}
 }
